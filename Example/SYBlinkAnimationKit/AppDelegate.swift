9f6ea293d5a4ef535b18ea3ea1242d18bb46b421
邓江洲 <707703386@qq.com>
2020-03-03 09:55:05 +0800

    Start

---
+++ Example/SYBlinkAnimationKit/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SYBlinkAnimationKit
+//
+//  Created by Shohei Yokoyama on 12/13/2015.
+//  Copyright (c) 2015 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  
+        
+//        let exampleViewController = ButtonViewController()
+//        let navigationController = UINavigationController(rootViewController: exampleViewController)
+//        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+//        self.window?.rootViewController = navigationController
+//        self.window?.makeKeyAndVisible()
+        return true
+    }
+    
+    
+
+}
+
