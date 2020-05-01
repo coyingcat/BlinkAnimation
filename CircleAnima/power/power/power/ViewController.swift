9011767e1f57270edb1de287c313885ac9952b33
邓江洲 <707703386@qq.com>
2020-05-01 13:04:41 +0800

    LineCap

---
+++ CircleAnima/power/power/power/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  power
+//
+//  Created by Jz D on 2020/5/1.
+//  Copyright © 2020 Jz D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let progressRing = { () -> UICircularProgressRing in
+        let ring = UICircularProgressRing(frame: CGRect(x: 100, y: 100, width: 250, height: 250))
+        ring.maxValue = 50
+        ring.style = .dashed(pattern: [7.0, 7.0])
+        
+        // Change any of the properties you'd like
+        return ring
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.addSubview(progressRing)
+    }
+
+    
+    
+    @IBAction func animate(_ sender: UIButton) {
+        
+        progressRing.startProgress(to: 30, duration: 2.0) {
+          print("Done animating!")
+          // Do anything your heart desires...
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+
+}
+
